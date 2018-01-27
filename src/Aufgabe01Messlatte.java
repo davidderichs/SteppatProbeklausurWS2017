d8c8e5fbfa8d263b9c1d24fff94d89cbe48d9cce
s57343 <s57343@beuth-hochschule.de>
2018-01-27 15:22:13 +0100

    hello world und pdf.

---
+++ src/Aufgabe01Messlatte.java
@@ -0,0 +1,9 @@
+/**
+ * Created by David Derichs on 27.01.2018.
+ */
+public class Aufgabe01Messlatte {
+
+    public static void main (String[] args){
+        System.out.println("blablubb");
+    }
+}
