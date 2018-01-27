7effc199f6c3f25826c1154eb6f787893b021c1f
s57343 <s57343@beuth-hochschule.de>
2018-01-27 20:28:36 +0100

    String.format() erklärung im Kommentar.

--- src/Aufgabe03Buchregister.java
+++ src/Aufgabe03Buchregister.java
@@ -26,6 +26,19 @@ public class Aufgabe03Buchregister{
         for (Map.Entry<String,Buch> eintrag : buecherListe.entrySet()){
             String titel = eintrag.getKey();
             Buch buch = eintrag.getValue();
+            // String.format Erklärung:
+            // String.format("......", parameter1, parameter 2 ...)
+            // "......" : Enthält den String, den man ausgeben will. Man kann da erst einmal alles reinschreiben.
+            // Will man die Werte aus den übergebenen Parametern an eine bestimmte Stelle schreiben, dann nutzt man
+            // den %-Operator. Dieser ersetzt sich selbst durch den Wert des jeweiligen Parameters.
+            // Dabei muss man schauen, der wievielte Parameter es ist.
+            // Also der erste %-Operator nimmt den wert des ersten übergebenen Parameters an.
+            // WICHTIG: Für verschiedene Typen bei den Übergabe-Parametern gibts auch verschiedene %-Operatoren.
+            //          %f - Float
+            //          %d - Dezimal (Integer)
+            //          %s - String
+            //          %4s - Die 4 Steht für die Mindestlaenge, Falls der Parameter zu klein ist wird mit Leerzeichen am Anfang aufgefüllt. Wie im CSS (Float: right)
+            //          %-4s - Dasselbe, nur das Die Leerzeichen am Ende aufgefüllt werden. Wie im CSS (Float: left)
             String output = String.format("%-10s -> [ ID: %-2d, Verfasser: %-15s, Signatur: %-15s ]%n",
                     titel,
                     buch.id,
