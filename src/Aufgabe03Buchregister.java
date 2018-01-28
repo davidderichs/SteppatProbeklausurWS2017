b55e461c509740102ebfd28a7b290f5a80a01343
s57343 <s57343@beuth-hochschule.de>
2018-01-28 21:42:29 +0100

    JunitTests.

--- src/Aufgabe03Buchregister.java
+++ src/Aufgabe03Buchregister.java
@@ -1,30 +1,28 @@
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by David Derichs on 27.01.2018.
- */
-
-class Buch {
-    public int id;
-    public String verfasser;
-    public String signatur;
-}
-
 /***
  * Buchregister speichert obere Klasse Buch als Datensatz in HashMap.
  */
 public class Aufgabe03Buchregister{
 
-    Map<String, Buch> buecherListe = new HashMap();
+    private Map<Integer, Buch> buecherListe = new HashMap();
 
-    public Buch sucheBuch(String titel){
-        return buecherListe.get(titel);
+    public Buch sucheBuch(int id){
+        return buecherListe.get(id);
+    }
+
+    public void add(Buch buch){
+        this.buecherListe.put(buch.id, buch);
+    }
+
+    public int size(){
+        return this.buecherListe.size();
     }
 
     public void gibAlleDatenAus(){
-        for (Map.Entry<String,Buch> eintrag : buecherListe.entrySet()){
-            String titel = eintrag.getKey();
+        for (Map.Entry<Integer,Buch> eintrag : buecherListe.entrySet()){
+            int id = eintrag.getKey();
             Buch buch = eintrag.getValue();
             // String.format Erklärung:
             // String.format("......", parameter1, parameter 2 ...)
@@ -40,43 +38,11 @@ public class Aufgabe03Buchregister{
             //          %4s - Die 4 Steht für die Mindestlaenge, Falls der Parameter zu klein ist wird mit Leerzeichen am Anfang aufgefüllt. Wie im CSS (Float: right)
             //          %-4s - Dasselbe, nur das Die Leerzeichen am Ende aufgefüllt werden. Wie im CSS (Float: left)
             String output = String.format("%-10s -> [ ID: %-2d, Verfasser: %-15s, Signatur: %-15s ]%n",
-                    titel,
-                    buch.id,
+                    id,
                     buch.verfasser,
                     buch.signatur
                     );
             System.out.print(output);
         }
-    }
-
-    /*** NICHT IN DER KLAUSUR:
-     * Test-Methode. Gehört später nicht zur Antwort in der Klausur.
-     * @param args
-     */
-    public static void main (String[] args){
-        Aufgabe03Buchregister register = new Aufgabe03Buchregister();
-        Buch buch = new Buch();
-        buch.id = 1;
-        buch.verfasser = "Verfasser1";
-        buch.signatur = "Signatur1";
-        String titel = "Titel1";
-        register.buecherListe.put(titel, buch);
-
-        buch = new Buch();
-        buch.id = 2;
-        buch.verfasser = "Verfasser2";
-        buch.signatur = "Signatur2";
-        titel = "Titel2";
-        register.buecherListe.put(titel, buch);
-
-        buch = new Buch();
-        buch.id = 11;
-        buch.verfasser = "Verfasser11";
-        buch.signatur = "Signatur11";
-        titel = "Titel11";
-        register.buecherListe.put(titel, buch);
-
-        register.gibAlleDatenAus();
-        System.out.println("Gesuchtes Buch Titel1 Signatur:" + register.sucheBuch("Titel1").signatur);
     }
 }
